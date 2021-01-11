5ffca9afc52839675152d727c77288f3782eb88a
Jéssica Bohn <jessicabohn74@gmail.com>
2021-01-10 23:51:12 -0300

    create Looney_Lips

---
+++ Looney_Lips/LooneyLips.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _ready():
+	var prompts = [ " André ", " beautiful", "world", "Jéssica", "love"]
+	var story = "Once upon a time a gentleman, called %s. He was the most %s prince in the %s. One day he met a commoner  called %s and fell in %s with her"
+	print(story % prompts)
+	prompts = [ " Kylo Ren ", " hideous", "world", "Rey", "hate"]
+	print(story % prompts)
+	
+
+	#person = " Someone Else "
+	#print("Once upon a time a gentle, called" + person + "the most beautiful prince in the world")
