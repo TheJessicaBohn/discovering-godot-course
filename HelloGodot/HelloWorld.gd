f38879f682361cb98e3a692ecd94939177ca9592
Jéssica Bohn <jessicabohn74@gmail.com>
2021-01-10 23:02:38 -0300

    create Hello World

---
+++ HelloGodot/HelloWorld.gd
@@ -0,0 +1,11 @@
+extends Node
+
+func _ready():
+	#var greeting =  "Hello "
+	#print(greeting + "World")
+	
+	#var number =  2
+	#print("5" + str(number))
+	var godot = "GODOT!"
+	print("What we are waiting for? " + godot)
+
