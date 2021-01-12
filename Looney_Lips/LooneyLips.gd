9292c04b22d8f11842d9af7e0c08e67c5f834c4e
Jéssica Bohn <jessicabohn74@gmail.com>
2021-01-11 22:35:56 -0300

    Updated Looney_Lips

--- Looney_Lips/LooneyLips.gd
+++ Looney_Lips/LooneyLips.gd
@@ -4,9 +4,9 @@ func _ready():
 	var prompts = [ " André ", " beautiful", "world", "Jéssica", "love"]
 	var story = "Once upon a time a gentleman, called %s. He was the most %s prince in the %s. One day he met a commoner  called %s and fell in %s with her"
 	print(story % prompts)
-	prompts = [ " Kylo Ren ", " hideous", "world", "Rey", "hate"]
-	print(story % prompts)
-	
-
+	#prompts = [ " Kylo Ren ", " hideous", "world", "Rey", "hate"]
+	#print(story % prompts)
 	#person = " Someone Else "
 	#print("Once upon a time a gentle, called" + person + "the most beautiful prince in the world")
+
+	$DisplayText.text = "Qualquer coisa na tela"
