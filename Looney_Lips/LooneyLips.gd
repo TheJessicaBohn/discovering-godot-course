16211900bb907a0598917ee7e12fa126b5407c55
Jéssica Bohn <jessicabohn74@gmail.com>
2021-01-13 22:13:56 -0300

    Updated Looney_Lips

--- Looney_Lips/LooneyLips.gd
+++ Looney_Lips/LooneyLips.gd
@@ -1,22 +1,50 @@
 extends Control
 
+var player_words = []
+var story = "Once upon a time a gentleman, called %s. He was the most %s prince in the %s. One day he met a commoner  called %s and fell in %s with her"
+var prompts = [ " a name ", " a noun", "adverb", "adjective"]
+
+onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText	
+onready var DisplayText = $VBoxContainer/DisplayText
+
 func _ready():
-	var prompts = [ " André ", " beautiful", "world", "Jéssica", "love"]
-	var story = "Once upon a time a gentleman, called %s. He was the most %s prince in the %s. One day he met a commoner  called %s and fell in %s with her"
-	print(story % prompts)
-	#prompts = [ " Kylo Ren ", " hideous", "world", "Rey", "hate"]
-	#print(story % prompts)
-	#person = " Someone Else "
-	#print("Once upon a time a gentle, called" + person + "the most beautiful prince in the world")
-	#$DisplayText.text = "Qualquer coisa na tela"
-	$VBoxContainer/DisplayText.text = story % prompts
-	
+	check_player_words_length()
+		
 	
 func _on_PlayerText_text_entered(new_text):
-	update_DisplayText(new_text)
+#	update_DisplayText(new_text)
+#	story = ""
+	add_to_player_words()	
 	
 	
-func update_DisplayText(words):
-	$VBoxContainer/DisplayText.text = words
-	$VBoxContainer/HBoxContainer/PlayerText.clear()
+func _on_TextureButton_pressed():
+#	var words = PlayerText.text
+#	update_DisplayText(words)
+	add_to_player_words()
+
+
+#func update_DisplayText(words):
+#	$VBoxContainer/DisplayText.text = words
+#	$VBoxContainer/HBoxContainer/PlayerText.clear()
+
+
+func add_to_player_words():
+	player_words.append(PlayerText.text)	
 	
+	
+func is_story_done():
+	return player_words.size() == prompts.size()
+		
+
+func check_player_words_length():]
+	if is_story_done():
+		tell_story()
+	else:
+		prompts_player()
+
+func tell_story():
+	DisplayText.text = story % player_words
+
+func prompts_player():	
+	DisplayText.text = "May I have"	+ prompts[player_words.size()] + " please?"
+							
