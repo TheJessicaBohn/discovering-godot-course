0fd9e02d75f80d24438447f5796350bf46d2b664
Jéssica Bohn <jessicabohn74@gmail.com>
2021-01-13 23:10:12 -0300

    Updated Looney_Lips, addind an array

--- Looney_Lips/LooneyLips.gd
+++ Looney_Lips/LooneyLips.gd
@@ -1,50 +1,46 @@
 extends Control
 
 var player_words = []
-var story = "Once upon a time a gentleman, called %s. He was the most %s prince in the %s. One day he met a commoner  called %s and fell in %s with her"
+var story = "Once upon a time someone called %s ate a %s flavoured sandwich which made him fell all %s inside. Is was a %s day"
 var prompts = [ " a name ", " a noun", "adverb", "adjective"]
 
 onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText	
 onready var DisplayText = $VBoxContainer/DisplayText
 
 func _ready():
+	DisplayText.text = "Welcome to Looney Lips! We're  going to tell a story and have a wonderful time!"
 	check_player_words_length()
 		
 	
-func _on_PlayerText_text_entered(new_text):
-#	update_DisplayText(new_text)
-#	story = ""
-	add_to_player_words()	
+func _on_PlayerText_text_entered(_new_text):
+	add_to_player_words()
 	
 	
 func _on_TextureButton_pressed():
-#	var words = PlayerText.text
-#	update_DisplayText(words)
 	add_to_player_words()
-
-
-#func update_DisplayText(words):
-#	$VBoxContainer/DisplayText.text = words
-#	$VBoxContainer/HBoxContainer/PlayerText.clear()
-
+	
 
 func add_to_player_words():
 	player_words.append(PlayerText.text)	
+	DisplayText.text = ""
+	PlayerText.clear()
+	check_player_words_length()
 	
 	
 func is_story_done():
 	return player_words.size() == prompts.size()
 		
 
-func check_player_words_length():]
+func check_player_words_length():
 	if is_story_done():
 		tell_story()
 	else:
-		prompts_player()
+		prompt_player()
+		
 
 func tell_story():
 	DisplayText.text = story % player_words
 
-func prompts_player():	
-	DisplayText.text = "May I have"	+ prompts[player_words.size()] + " please?"
+func prompt_player():	
+	DisplayText.text += "May I have " + prompts[player_words.size()] + " please?"
 							
