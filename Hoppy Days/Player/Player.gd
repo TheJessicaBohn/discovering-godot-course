0d58c474f54b786ce52d2ba53f7d1b227f6a45f3
Jéssica Bohn <jessicabohn74@gmail.com>
2021-01-31 22:02:06 -0300

    Add Sound Efects and Background Music

--- Hoppy Days/Player/Player.gd
+++ Hoppy Days/Player/Player.gd
@@ -34,7 +34,8 @@ func apply_gravity():
 func jump():
 	if Input.is_action_pressed("jump") and is_on_floor():
 		motion.y -= JUMP_SPEED
-	
+		#$AudioStreamPlayer.stream = load("res://SFX/jump1.ogg")
+		$JumpSFX.play()
 	
 func move():		
 	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
@@ -57,5 +58,7 @@ func hurt():
 	yield(get_tree(), "idle_frame")	
 	motion.y -= JUMP_SPEED
 	lives -= 1
+	#$AudioStreamPlayer.stream = load("res://SFX/pain.ogg")
+	$PainSFX.play()
 	if lives < 0:
 		end_game()
