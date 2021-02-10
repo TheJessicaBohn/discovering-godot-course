3a6eac83c80cf7beb1a2d7978cdcdbef4680ea6c
jessica bohn <jessicabohn74@gmail.com>
2021-02-09 23:42:29 -0300

    Update GUI counting lives

--- Hoppy Days/Levels/GameState.gd
+++ Hoppy Days/Levels/GameState.gd
@@ -1,17 +1,21 @@
 extends Node2D
 
-var lives = 3
+var lives = 5
 
 
 func _ready():
 	add_to_group("GameState")
+	update_GUI()
 
 func hurt():
 	lives -= 1
 	$Player.hurt()
-	#if lives < 0 end the game
+	update_GUI()
 	if lives < 0:
 		end_game() 
+		
+func update_GUI():
+		get_tree().call_group("GUI", "update_lives", lives)		
 		
 func end_game():
 	get_tree().change_scene("res://Levels/GameOver.tscn")		
