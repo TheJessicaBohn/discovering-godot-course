5402f498ef964939f559ebd11f5a976d72fcca44
jessica bohn <jessicabohn74@gmail.com>
2021-02-14 21:10:47 -0300

    Fixing bug is_on_the floor for bunny jumps

--- Hoppy Days/Player/Player.gd
+++ Hoppy Days/Player/Player.gd
@@ -22,7 +22,7 @@ func _physics_process(delta):
 func apply_gravity():
 	if position.y > WORLD_LIMIT:
 		get_tree().call_group("GameState", "end_game")
-	if is_on_floor():
+	if is_on_floor() and motion.y > 0:
 		motion.y = 0
 	elif is_on_ceiling():
 		motion.y = 1	
@@ -32,8 +32,7 @@ func apply_gravity():
 		
 func jump():
 	if Input.is_action_pressed("jump") and is_on_floor():
-		motion.y -= JUMP_SPEED
-		#$AudioStreamPlayer.stream = load("res://SFX/jump1.ogg")
+		motion.y = -JUMP_SPEED
 		$JumpSFX.play()
 	
 func move():		
@@ -52,13 +51,13 @@ func animate():
 func hurt():
 	position.y -= 1
 	yield(get_tree(), "idle_frame")	
-	motion.y -= JUMP_SPEED
+	motion.y = -JUMP_SPEED
 	$PainSFX.play()
 		
 func boost():
 	position.y -= 1
 	yield(get_tree(), "idle_frame")	
-	motion.y -= JUMP_SPEED * BOOSTER_MULTIPLIER
+	motion.y = -JUMP_SPEED * BOOSTER_MULTIPLIER
 
 	
 	
